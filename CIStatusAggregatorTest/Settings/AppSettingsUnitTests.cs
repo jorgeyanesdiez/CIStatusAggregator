e12716bc1fa8f21c31000f52af6d210d02dde6aa
Jorge Yanes Diez <jorgeyanesdiez@users.noreply.github.com>
2023-02-05 12:44:50 +0100

    Adds missing test.

---
+++ CIStatusAggregatorTest/Settings/AppSettingsUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CIStatusAggregator.Settings
+{
+
+    [Trait("TestType", "Unit")]
+    public class AppSettingsUnitTests
+    {
+
+        [Fact]
+        public void CIStatusAggregator_ByDefault_IsNotNull()
+        {
+            var sut = new AppSettings();
+            sut.CIStatusAggregator.Should().NotBeNull();
+        }
+
+    }
+}
